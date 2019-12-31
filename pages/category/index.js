ca29982e492e387d34149a4cce05c89905f97e9e
代罔 <1403693160@qq.com>
2019-12-31 15:12:00 +0800

    category index done

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,38 @@
+import { request } from '../../request/index.js'
 // pages/category/index.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        leftMenuList: [],
+        rightContent: [],
+        currentIndex: 0
+    },
+    Cates: [],
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        this.getCategorys()
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    getCategorys() {
+        request({ url: 'https://api.zbztb.cn/api/public/v1/categories' })
+            .then((result) => {
+                this.Cates = result.data.message
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+                // 构造左侧的大菜单数据
+                let leftMenuList = this.Cates.map(v => v.cat_name)
+                    // 构造右侧
+                    // let rightContent = this.Cates.map(c => c.children)
+                let rightContent = this.Cates[0].children
+                this.setData({
+                    leftMenuList,
+                    rightContent
+                })
+            })
+    }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,38 +1,56 @@
+import { request } from '../../request/index.js'
 //Page Object
 Page({
-  data: {
-    
-  },
-  //options(Object)
-  onLoad: function(options){
-    
-  },
-  onReady: function(){
-    
-  },
-  onShow: function(){
-    
-  },
-  onHide: function(){
+    data: {
+        swiperList: [],
+        catesList: [],
+        floorList: []
+    },
+    //options(Object)
+    onLoad: function(options) {
+        // 1.传统方法，发送ajax请求
+        // wx.request({
+        //     url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+        //     success: (result) => {
+        //         this.setData({
+        //             swiperList: result.data.message
+        //         })
+        //     },
+        // });
 
-  },
-  onUnload: function(){
+        // 2.promise方法
+        this.getSwiper()
+        this.getCates()
+        this.getFloor()
 
-  },
-  onPullDownRefresh: function(){
 
-  },
-  onReachBottom: function(){
 
-  },
-  onShareAppMessage: function(){
-
-  },
-  onPageScroll: function(){
-
-  },
-  //item(index,pagePath,text)
-  onTabItemTap:function(item){
-
-  }
+    },
+    // 获取轮播图的方法
+    getSwiper() {
+        request({ url: "https://api.zbztb.cn/api/public/v1/home/swiperdata" })
+            .then(result => {
+                this.setData({
+                    swiperList: result.data.message
+                })
+            })
+    },
+    // 获取分类
+    getCates() {
+        request({ url: "https://api.zbztb.cn/api/public/v1/home/catitems" })
+            .then(result => {
+                this.setData({
+                    catesList: result.data.message
+                })
+            })
+    },
+    // 获取楼层
+    getFloor() {
+        request({ url: "https://api.zbztb.cn/api/public/v1/home/floordata" })
+            .then(result => {
+                this.setData({
+                    floorList: result.data.message
+                })
+            })
+    }
 });
